7e9a1a3b8db485377fefcddae082817545d01149
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-21 22:19:27 +0530

    Update sample1.java

--- practice programming/prgm 1/sample1.java
+++ practice programming/prgm 1/sample1.java
@@ -6,7 +6,7 @@ String Name = sc.nextLine();
 char a = sc.next().charAt(0);
 char b = sc.next().charAt(0);
 int count = 0;
-for(int i = 0; i<Name.length();i++){
+for(int i = 0; i<Name.length()-1;i++){
 if(Name.charAt(i)==a && Name.charAt(i+1)=b){
 count++;
 }
