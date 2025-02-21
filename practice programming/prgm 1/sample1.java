cb24ab56dd10e74763903ba4835bff3bd9af0840
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-21 22:01:32 +0530

    Update sample1.java

--- practice programming/prgm 1/sample1.java
+++ practice programming/prgm 1/sample1.java
@@ -1,1 +1,16 @@
-
+import java.util.*;
+public class Hello (
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+String Name = sc.nextLine();
+char a = sc.next().charAt(0);
+char b = sc.next().charAt(0);
+int count = 0;
+for(int i = 0; i<Name.length();i++){
+if(Name.charAt(i)==a && Name.charAt(i+1)=b){
+count++;
+}
+}
+System.out.print(count + "1");
+}
+}
