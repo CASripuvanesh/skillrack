4b7f9b2c57ca11d1c1a954543858a19f4ba9011c
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-22 22:07:36 +0530

    Update sample.java

--- practice programming/prgm2/sample.java
+++ practice programming/prgm2/sample.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] times = sc.nextLine().split(" ");
+        sc.close();
+        int count = 0;
 
+        for (String time : times) {
+            String[] parts = time.split(":");
+            int hours = Integer.parseInt(parts[0]);
+            int minutes = Integer.parseInt(parts[1]);
+            if (hours > 10 || (hours == 10 && minutes > 0)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
