25bf69f6827f47e6cdbb650961108cf2602ed67d
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-23 13:52:24 +0530

    Update sample.java

--- practice programming/prgm4/sample.java
+++ practice programming/prgm4/sample.java
@@ -1,1 +1,12 @@
-
+class sample{
+  public static void main(String[] args){
+    Scanner sc =new Scanner(Sytem.in);
+    String Str = sc.nextLine();
+    String Str1 = sc.nextLine();
+    if(Str.compareTo(Str1)<0){
+      System.out.print(Str+Str1);
+    }else{
+      System.out.print(Str1+str);
+    }
+  }
+}
