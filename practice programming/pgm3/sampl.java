c0ad78e45b7af1907b6109421437edce7271b059
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-23 12:57:34 +0530

    Update sampl.java

--- practice programming/pgm3/sampl.java
+++ practice programming/pgm3/sampl.java
@@ -1,1 +1,12 @@
-
+public class sampl{
+  public static void main(String[] args){
+    Scanner sc =  new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int C = sc.nextInt();
+    int Length = sc.nextInt();
+    int diff = B-A;
+    int Nthformula  = A+(Length-1)*diff;
+    System.out.print(Nthformula);
+  }
+}
