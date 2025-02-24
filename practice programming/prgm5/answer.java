dc627d4302656a11b2aa42dc71e5a3b9863ac8ee
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-25 00:20:54 +0530

    Update answer.java

--- practice programming/prgm5/answer.java
+++ practice programming/prgm5/answer.java
@@ -1,1 +1,18 @@
-
+public class Answer{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String S = sc.nextLine();
+    String[] Arr = S.split(" ");
+    StringBuilder result = new StringBuilder();
+    for(String first:Arr){
+      StringBuilder modify =new StringBuilder();
+      for(int i = 0;i< first.length();i++){
+        if(i%2==0){
+          modify.append(Character.toUpperCase(first.chatAt(i)));
+        }else{
+          modify.append(Character.toLowerCase(first.charAt(i)));
+    }
+  } return.append(modify.append(" "));
+    } System.out.print(result.toString);
+  }
+}
