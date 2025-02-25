51267581719ec2b55c379d00c32812635fb99c01
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-25 22:33:03 +0530

    Update Main.java

--- practice programming/prgm6/Main.java
+++ practice programming/prgm6/Main.java
@@ -1,1 +1,14 @@
-
+public class Main{
+  public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    String Str = sc.nextLine();
+    int index = Str.indexOf('_');
+    if(index == -1){
+      System.out.print(new StringBuilder(Str).reverse().toString());
+    }else{
+      StringBuilder rev = new StringBuilder(Str.substring(0,index)).reverse().toString();
+      String Bal = Str.substring(index+1);
+      System.out.print(rev + "_"+ Bal);
+    }
+  }
+}
