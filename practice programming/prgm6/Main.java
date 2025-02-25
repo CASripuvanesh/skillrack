1a2d279e010017d58b5b8e8c5e2828a905c4f323
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-25 22:39:50 +0530

    Update Main.java

--- practice programming/prgm6/Main.java
+++ practice programming/prgm6/Main.java
@@ -6,7 +6,7 @@ public class Main{
     if(index == -1){
       System.out.print(new StringBuilder(Str).reverse().toString());
     }else{
-      StringBuilder rev = new StringBuilder(Str.substring(0,index)).reverse().toString();
+      String rev = new StringBuilder(Str.substring(0,index)).reverse().toString();
       String Bal = Str.substring(index+1);
       System.out.print(rev + "_"+ Bal);
     }
