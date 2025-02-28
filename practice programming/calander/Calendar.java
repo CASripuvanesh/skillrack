233eebf848e854a64c623d67031f31ee20cf38d5
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-02-28 14:36:54 +0530

    Create Calendar.java

---
+++ practice programming/calander/Calendar.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+public class Calendar{
+        public static void main(String[] args) {
+            Calendar cal = new GregorianCalendar();
+            System.out.println(cal.getTime());
+        }
+    }
