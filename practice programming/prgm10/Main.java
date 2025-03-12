fb3ebada4f6405f38b3c06d337b6c2971c54bbc5
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-03-12 13:46:23 +0530

    Create Main.java

---
+++ practice programming/prgm10/Main.java
@@ -0,0 +1,21 @@
+// TO print the next prime number of the input 
+// input = 2
+// output = 3
+
+public class Main{
+    public static boolean prime(int num){
+        if(num<2) return false;
+        for(int i =2;i<=num/2;i++){
+            if(num%2==0) return false;
+        } return true;
+}
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            int prime = N+1;
+            while(! prime(prime)){
+                prime++;
+            }
+            System.out.print(prime);
+        }
+    }
