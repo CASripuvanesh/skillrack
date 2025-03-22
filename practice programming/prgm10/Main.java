b4128c733f321628bb3d538e6352661a3ba7c50a
CASripuvanesh <121247310+CASripuvanesh@users.noreply.github.com>
2025-03-22 11:47:02 +0530

    Update Main.java

--- practice programming/prgm10/Main.java
+++ practice programming/prgm10/Main.java
@@ -6,7 +6,7 @@ public class Main{
     public static boolean prime(int num){
         if(num<2) return false;
         for(int i =2;i<=num/2;i++){
-            if(num%2==0) return false;
+            if(num%i==0) return false;
         } return true;
 }
         public static void main(String[] args){
